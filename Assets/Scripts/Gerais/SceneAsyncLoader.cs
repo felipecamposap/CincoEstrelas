298d508645d4c19b1c5f94e749e6ca12dde64d96
IgorItaloSilva <igoritalosilva0@gmail.com>
2024-05-10 09:54:12 -0300

    Implementado cinematica e carregamento paralelo

---
+++ Assets/Scripts/Gerais/SceneAsyncLoader.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneAsyncLoader : MonoBehaviour
+{
+    AsyncOperation asyncLoad;
+    [SerializeField] Button skipButtom;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int indexScene;
+        try { indexScene = SceneManager.GetActiveScene().buildIndex + 1; }
+        catch { indexScene = 0; }
+        asyncLoad = SceneManager.LoadSceneAsync(indexScene);
+        asyncLoad.allowSceneActivation = false;
+        StartCoroutine(SceneIsDone());
+    }
+
+    private IEnumerator SceneIsDone()
+    {
+        yield return new WaitForSeconds(1);
+        if (asyncLoad.progress >= 0.9f)
+            skipButtom.gameObject.SetActive(true);
+        else
+            StartCoroutine(SceneIsDone());
+        Debug.Log("Load Progress " + asyncLoad.progress);
+    }
+
+    public void LoadScene()
+    {
+        Debug.Log("Carregar");
+        asyncLoad.allowSceneActivation = true;
+    }
+}
